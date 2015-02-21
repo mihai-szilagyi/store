125fa86d6686ac85a4ee470ecbaec7ffb7213b1e
mihai.szilagyi <mihai.szilagyi@gmail.com>
2015-02-21 20:35:03 +0200

    add model + json config file

--- store_app/lib/StoreApp.pm
+++ store_app/lib/StoreApp.pm
@@ -1,5 +1,6 @@
 package StoreApp;
 use Mojo::Base 'Mojolicious';
+use Model::Schema;
 
 # This method will run once at server start
 sub startup {
@@ -7,7 +8,21 @@ sub startup {
 
     # Documentation browser under "/perldoc"
     $self->plugin('PODRenderer');
-    $self->plugin('Config');
+    my $config = $self->plugin('JSONConfig');
+
+    my $dsn = "dbi:mysql:" . $config->{database}->{dbname};
+      
+    unless ($self->can('model')) {
+        __PACKAGE__->attr(
+         'model' => sub {
+                Model::Schema->connect(
+                    $dsn,  
+                    $config->{database}->{username},
+                    $config->{database}->{password},
+                );
+            }
+        );
+    }
     # Router
     my $r = $self->routes;
 
