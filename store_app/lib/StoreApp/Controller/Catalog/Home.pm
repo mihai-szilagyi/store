861ffb85dce88af10af7fd91767718f6f3c837ac
mihai.szilagyi <mihai.szilagyi@gmail.com>
2015-02-23 19:09:58 +0200

    format carusel

--- store_app/lib/StoreApp/Controller/Catalog/Home.pm
+++ store_app/lib/StoreApp/Controller/Catalog/Home.pm
@@ -12,18 +12,18 @@ sub home {
 sub _carousel {
 	my $self = shift;
 	my $model = $self->app->model;
-	my $products  =$model->resultset('Carousel')->search({}, {
+	my $slides  =$model->resultset('Carousel')->search({}, {
 		order_by => { -asc => "position" },
    		});
 
 	my @carousel = ();
 
-	while (my $product = $products->next) {
-		my $id = $product->id;
+	while (my $slide = $slides->next) {
+		my $id = $slide->product->id;
 		my $path = $self->config->{path}->{images};
 		my @images = glob "'${path}/*${id}.*'";
 		my $image = scalar @images ? "/images/" . basename ( $images[0] ) : "http://placehold.it/350x150";
-		push @carousel, {$id => $image};
+		push @carousel, { id => $id , image => $image, caption => $slide->product->description() };
 	}
 
 	$self->stash(carousel => \@carousel);
